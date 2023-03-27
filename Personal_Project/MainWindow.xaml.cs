6600b70a1006331d652d26dea668376933483aee
Adam Grace <S00188523@atu.iem>
2023-03-27 16:53:52 +0100

    API call Created, Class created and initialized

--- Personal_Project/MainWindow.xaml.cs
+++ Personal_Project/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Newtonsoft;
+
 
 namespace Personal_Project
 {
@@ -23,6 +17,50 @@ namespace Personal_Project
         public MainWindow()
         {
             InitializeComponent();
+            PopulateStandingsListBox();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Standings> standings = new List<Standings>();
+            lbxTable.ItemsSource = standings;
+
+        }
+
+        private async void PopulateStandingsListBox()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://premier-league-standings1.p.rapidapi.com/"),
+                Headers =
+                {
+                 { "X-RapidAPI-Key", "d99ef4d195mshf7ef03579a02ac0p1916b4jsn5524649b62a4" },
+                     { "X-RapidAPI-Host", "premier-league-standings1.p.rapidapi.com" },
+                },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON response into a list of Standings objects
+                var standings = JsonSerializer.Deserialize<List<Standings>>(json);
+
+                // Filter out any standings with null values
+                standings = standings.Where(s => s.TeamName != null && s.Played != null && s.Won != null && s.Drawn != null && s.Lost != null && s.Points != null).ToList();
+
+                // Sort the standings by points
+                standings = standings.OrderByDescending(s => s.Points).ToList();
+
+                // Bind the standings list to the ListBox
+                lbxTable.ItemsSource = standings;
+            }
+        }
+
+
     }
+
 }
